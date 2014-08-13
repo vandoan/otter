a00b28da4d50c8f24ace64ba325dddc00b2f9873
Van Doan <vdoan09@gmail.com>
2014-08-13 02:28:31 -0400

    migrated statuses, created user_id, first name, last name, profile name

---
+++ db/migrate/20140813062336_add_user_it_to_statuses.rb
@@ -0,0 +1,8 @@
+class AddUserItToStatuses < ActiveRecord::Migration
+  def change
+  	add_column :statuses, :user_id, :integer
+  	add_index :statuses, :user_id 
+  	remove_column :statuses, :name
+
+  end
+end
