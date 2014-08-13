d8244190af91a21cfdaa4f4cd9691135cf37e850
Van Doan <vdoan09@gmail.com>
2014-08-13 11:09:58 -0400

    added simple_form gem, stylized status page

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
+gem 'simple_form'
 gem 'devise' 
 gem 'twitter-bootstrap-rails'
 gem 'less-rails' 
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :statuses
 
-  root to: 'status#index'
+  root to: 'statuses#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
