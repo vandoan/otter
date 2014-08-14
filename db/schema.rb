8fafe1af341a3a83325051da89d41dbe58dfdd0e
Van Doan <vdoan09@gmail.com>
2014-08-14 18:44:59 -0400

    listed a series of issues that need to be addressed, stylized the log in and out, took out broken parts in the statuses and application.html.erb.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140813062336) do
+ActiveRecord::Schema.define(version: 20140813154232) do
 
   create_table "statuses", force: true do |t|
     t.text     "content"
@@ -19,8 +19,6 @@ ActiveRecord::Schema.define(version: 20140813062336) do
     t.datetime "updated_at"
     t.integer  "user_id"
   end
-
-  add_index "statuses", ["user_id"], name: "index_statuses_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "first_name"
