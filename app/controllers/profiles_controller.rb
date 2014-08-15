37ed5f0d8306ed80ed23e34661f2ee31bac70352
Van Doan <vdoan09@gmail.com>
2014-08-15 05:54:37 -0400

    modified user's profile page so that it prints his or her name and statuses. There's a bug in the profile controller page.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,6 +2,10 @@ class ProfilesController < ApplicationController
   def show
   	@user = User.find_by_profile_name(params[:id])
   	if @user
+  		@status = Status.all
+  		#{should be @statuses = @user.statuses.all, but broken }
+
+
   		render action: :show 
   	else 
   		render file: 'public/404', status: 404, formats: [:html]
