fb47b9e2f8dbabc816eb24f4a85e99b648e8e901
Van Doan <vdoan09@gmail.com>
2014-08-15 22:45:09 -0400

    finally solved the bug with the profile show page and profile controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,9 +2,9 @@ class ProfilesController < ApplicationController
   def show
   	@user = User.find_by_profile_name(params[:id])
   	if @user
-  		@status = Status.all
+      @statuses = @user.statuses.order("created_at DESC").all
   		#{should be @statuses = @user.statuses.all, but broken, scoping issue }
-
+      # (created at DESC puts the earliest post first)
   		render action: :show 
   	else 
   		render file: 'public/404', status: 404, formats: [:html]
