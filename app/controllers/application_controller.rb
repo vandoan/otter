f3bf982f932b1667d46139c3e3e9b36909a963ea
Van Doan <vdoan09@gmail.com>
2014-08-17 02:26:31 -0400

    spent over an hour trying to figure out how to insert a default for a input box with a collection, failed at that, will return another day to it, generated a hobbies column, deleted Users to find a bug, but the bug turned out to be in the syntax of the migration, found it when checking the schema.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,9 +13,9 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :profile_name, :favorite_books, :email, :password, :password_confirmation, :remember_me) }
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :hobbies, :last_name, :profile_name, :favorite_books, :email, :password, :password_confirmation, :remember_me) }
     devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :username, :email, :password, :remember_me) }
-    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :profile_name, :favorite_books, :current_password) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :hobbies, :email, :password, :password_confirmation, :profile_name, :favorite_books, :current_password) }
   end
 
 end
