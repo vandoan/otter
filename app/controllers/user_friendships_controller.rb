504d7da231611dedd562c28be61b6cfd315834ca
Van Doan <vdoan09@gmail.com>
2014-09-16 22:58:51 -0400

    the add friend button is now working but is still buggy

--- app/controllers/user_friendships_controller.rb
+++ app/controllers/user_friendships_controller.rb
@@ -1,115 +1,55 @@
 class UserFriendshipsController < ApplicationController
-  before_filter :authenticate_user!
-  before_action :set_user_friendship, only: [:accept, :destroy]
-  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
-  respond_to :html, :json
-
-  def index
-    @user_friendships = UserFriendshipDecorator.decorate_collection(friendship_association.all)
-    respond_with @user_friendships
-  end
+	rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+	before_action :authenticate_user!
 
 
-  def new
-    if params[:friend_id]
-      @friend = User.where(profile_name: params[:friend_id]).first
-      raise ActiveRecord::RecordNotFound if @friend.nil?
-      @user_friendship = current_user.user_friendships.new(friend: @friend)
-    else
-      flash[:error] = "Friend required"
-    end
-  end
+	def index
+		@user_friendships = current_user.user_friendships.all
+	end
 
 
-  def edit
-    @friend = User.where(profile_name: params[:id]).first   
-    @user_friendship = current_user.user_friendships.where(friend_id: @friend.id).first.decorate
-  end
+	def new
+		if params[:friend_id]
+			@friend = User.where(profile_name: params[:friend_id]).first
+			raise ActiveRecord::RecordNotFound if @friend.nil?
+			@user_friendship = current_user.user_friendships.new(friend: @friend)
+		else
+			flash[:error] = "Friend required"
+		end
+	end
 
 
-  def create
-    if params[:user_friendship] && params[:user_friendship].has_key?(:friend_id)    
-      @friend = User.where(profile_name: params[:user_friendship][:friend_id]).first
-      @user_friendship = UserFriendship.request(current_user, @friend)
-      respond_to do |format|
-        if @user_friendship.new_record?
-          format.html do
-            flash[:error] = "There was a problem creating that friendship"
-            redirect_to profile_path(@friend)
-          end
-          format.json { render json: @user_friendship.to_json, status: :precondition_failed }
-        else
-          format.html do
-            flash[:success] = "Friend request sent."
-            redirect_to profile_path(@friend)
-          end 
-          format.json { render json: @user_friendship.to_json }       
-        end
-      end
-    else
-      flash[:error] = "Friend required"
-      redirect_to root_path
-    end
-  end
-
-
-  def accept
-    if @user_friendship.accept!
-      flash[:success] = "You are now friends with #{@user_friendship.friend.first_name}"
-    else
-      flash[:success] = "That friendship could not be accepted"
-    end
-    redirect_to user_friendships_path
-  end
-
-
-  def block
-    @user_friendship = current_user.user_friendships.find(params[:id])
-    if @user_friendship.block!
-      flash[:success] = "You have blocked #{@user_friendship.friend.first_name}"
-    else
-      flash[:error] = "That person could not be blocked"
-    end
-    redirect_to user_friendships_path
-  end
-
-
-  def destroy
-    if @user_friendship.destroy     
-      flash[:success] = "You are no longer friends"
-    end
-    redirect_to user_friendships_path
-  end
+	def create
+		if params[:user_friendship] && params[:user_friendship].has_key?(:friend_id)
+			if user_friendship_params			
+				@friend = User.where(profile_name: params[:user_friendship][:friend_id]).first
+				@user_friendship = current_user.user_friendships.new(friend: @friend)
+				@user_friendship.save
+				flash[:success] = "You are now friends with #{@friend.full_name}"
+				redirect_to profile_path(@friend)
+			else
+				redirect_to root_path
+			end
+		else
+				flash[:error] = "Friend required"
+				redirect_to root_path
+		end
+	end
 
 
 
-  private
 
-  def friendship_association
-    case params[:list]
-    when nil
-      current_user.user_friendships
-    when 'blocked'
-      current_user.blocked_user_friendships
-    when 'pending'  
-      current_user.pending_user_friendships
-    when 'accepted'
-      current_user.accepted_user_friendships
-    when 'requested'
-      current_user.requested_user_friendships
-    end 
-  end
 
-  def record_not_found
-    render file: 'public/404', status: :not_found
-  end
 
-  def set_user_friendship
-    @user_friendship = current_user.user_friendships.find(params[:id])
-  end
+	private
+
+	def record_not_found
+		render file: 'public/404', status: :not_found
+	end
+
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_friendship_params
-        params.require(:user_friendship).permit(:id, :user_id, :friend_id)
+        params.require(:user_friendship).permit(:friend_id)
     end
 end
